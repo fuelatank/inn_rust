b053d413c3f12a09d4fe5379040f439178c02a82
Atank Fuel <fuelatank@gmail.com>
2022-01-23 07:28:47 +0800

    first version, with ExecutingState, but the tests is filled by errors

--- src/flow.rs
+++ src/flow.rs
@@ -1,44 +1,70 @@
 
 use crate::enums::Color;
 use crate::enums::Splay;
+use crate::card::Card;
 
-pub enum Place {
-    Hand,
-    ScorePile,
-    Stack(Color),
-    Board
+enum ExecutingState<'a> {
+    Done,
+    ChooseAnyCard {
+        min_num: u8,
+        max_num: Option<u8>,
+        from: Vec<&'a Card>,
+        callback: Box<dyn Fn(Option<Vec<&'a Card>>) -> ExecutingState<'a>>
+    }
 }
 
-pub enum CrossPlayer {
-    Me,
-    You
-}
-
-pub enum Age {
-    Number(u8),
-    Highest()
-}
-
-pub enum FlowElem {
-    Draw(u8),
-    DrawAndMeld(u8),
-    DrawAndTuck(u8),
-    DrawAndScore(u8),
-    Splay(Splay),
-    MayElse(Flow, Flow),
-}
-
-type Flow = Vec<FlowElem>;
+type Flow<'a> = Box<dyn Fn() -> ExecutingState<'a>>;
 
 mod tests {
+    use crate::game::transfer_elem;
+use crate::containers::VecSet;
+use crate::game::Game;
+use crate::containers::Addable;
     use crate::game::Player;
-use crate::card::Achievement;
+    use crate::card::{Card, Achievement};
     use crate::containers::CardSet;
-    use crate::card::Card;
-    use crate::game::Game;
     use super::*;
+
+    fn chemistry2<T: CardSet<Card>, U: Addable<Achievement> + Default>() -> Box<dyn Fn(Player<T, U>)> {
+        // Player is inside Game
+        // One player must be placed inside one game
+        // Player is created when that Game is created
+        Box::new(
+            |player| {
+                player.draw_and_score(player.age() + 1);
+            }
+        )
+    }
+
+    fn opticsxx<T: CardSet<Card>, U>() -> Box<dyn Fn(Player<T, U>) -> ExecutingState>
+    where 
+        U: Addable<Achievement> + Default
+    {
+        Box::new(
+            |player| {
+                let card = player.draw_and_meld(3);
+                if card.contains(Icon::Crown) {
+                    player.draw_and_score(4);
+                    return ExecutingState::Done;
+                } else {
+                    return ExecutingState::ChooseAnyCard {
+                        min_num: 1,
+                        max_num: Some(1),
+                        from: player.score_pile(),
+                        callback: |cards| {
+                        return ChooseAnOpponent(|opponent| {
+                            transfer_elem(player.score_pile(), opponent.score_pile(), cards[0])
+                        })}
+                    };
+                }
+            }
+        )
+    }
+
     #[test]
     fn name() {
+        let game: Game<VecSet<Card>, VecSet<Achievement>> = Game::new();
+        game.add_player();
         let the_wheel = vec![
             FlowElem::Draw(1),
             FlowElem::Draw(1)
@@ -46,16 +72,8 @@ use crate::card::Achievement;
         let chemistry1 = vec![
             FlowElem::MayElse(vec![FlowElem::Splay(Splay::Right)], vec![])
         ];
-        fn f<T: CardSet<Card>, U: CardSet<Achievement>>() -> Box<dyn Fn(Game<T, U>, Player<T, U>)> {
-            // Player is inside Game
-            // One player must be placed inside one game
-            // Player is created when that Game is created
-            Box::new(
-                |game: Game<T, U>, player: Player<T, U>| {
-                    player.draw(game.main_card_pile, player.age() + 1)
-                }
-            )
-        }
-        let chemistry2 = || {};
+        let optics = vec![
+            FlowElem::DrawAndMeld(3)
+        ];
     }
 }
