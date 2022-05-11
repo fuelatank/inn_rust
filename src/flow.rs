e1f5b40460faa288c09f54f314afd25f9a986dce
Atank Fuel <fuelatank@gmail.com>
2022-05-11 13:58:25 +0800

    used Option instead of including Done to allow ? operator

--- src/flow.rs
+++ src/flow.rs
@@ -17,7 +17,22 @@ pub enum ExecutingState<'a> {
     },
     ChooseYn {
         callback: Box<dyn FnOnce(bool) -> ExecutingState<'a> + 'a>,
-    }
+    },
+}
+
+pub enum State<'a> {
+    Card {
+        min_num: u8,
+        max_num: Option<u8>,
+        from: Vec<&'a Card>,
+        callback: Box<dyn FnOnce(Vec<&'a Card>) -> Option<State<'a>> + 'a>,
+    },
+    Opponent {
+        callback: Box<dyn FnOnce(&'a Player<'a>) -> Option<State<'a>> + 'a>,
+    },
+    Yn {
+        callback: Box<dyn FnOnce(bool) -> Option<State<'a>> + 'a>,
+    },
 }
 
 pub enum Yield<'a> {
@@ -29,17 +44,17 @@ pub enum Yield<'a> {
     ChooseAnOpponent,
 }
 
-pub type ShareFlow = Box<dyn for<'a> Fn(&'a Player<'a>, &'a Game<'a>) -> ExecutingState<'a>>;
+pub type ShareFlow = Box<dyn for<'a> Fn(&'a Player<'a>, &'a Game<'a>) -> Option<State<'a>>>;
 pub type DemandFlow =
-    Box<dyn for<'a> Fn(&'a Player<'a>, &'a Player<'a>, &'a Game<'a>) -> ExecutingState<'a>>;
+    Box<dyn for<'a> Fn(&'a Player<'a>, &'a Player<'a>, &'a Game<'a>) -> Option<State<'a>>>;
 
 mod tests {
     //use crate::game::transfer_elem;
     use super::*;
-    use crate::enums::Splay;
     use crate::card::Achievement;
     use crate::containers::Addable;
     use crate::containers::VecSet;
+    use crate::enums::Splay;
     use crate::game::transfer;
     use crate::game::Player;
 
@@ -57,7 +72,7 @@ mod tests {
         Box::new(|player, opponent, _game| {
             opponent.draw(1);
             let age = opponent.age();
-            return ExecutingState::ChooseAnyCard {
+            Some(State::Card {
                 min_num: 1,
                 max_num: Some(1),
                 from: opponent
@@ -69,9 +84,9 @@ mod tests {
                     .collect(),
                 callback: Box::new(move |cards: Vec<&Card>| {
                     transfer(&opponent.hand, &player.hand, cards[0]);
-                    return ExecutingState::Done;
+                    None
                 }),
-            };
+            })
         })
     }
 
@@ -80,35 +95,29 @@ mod tests {
             let card = player.draw_and_meld(3).unwrap();
             if card.contains(Icon::Crown) {
                 player.draw_and_score(4);
-                return ExecutingState::Done;
+                return None;
             } else {
-                return ExecutingState::ChooseAnyCard {
+                return Some(State::Card {
                     min_num: 1,
                     max_num: Some(1),
                     from: player.score_pile.borrow().as_vec(),
                     callback: Box::new(move |cards: Vec<&Card>| {
-                        match cards.get(0) {
-                            Some(&card) => ExecutingState::ChooseAnOpponent {
-                                callback: Box::new(move |opponent: &Player| {
-                                    transfer(
-                                        &player.score_pile,
-                                        &opponent.score_pile,
-                                        card,
-                                    );
-                                    ExecutingState::Done
-                                }),
-                            },
-                            None => ExecutingState::Done
-                        }
+                        let card = *cards.get(0)?;
+                        Some(State::Opponent {
+                            callback: Box::new(move |opponent: &Player| {
+                                transfer(&player.score_pile, &opponent.score_pile, card);
+                                None
+                            }),
+                        })
                     }),
-                };
+                });
             }
         })
     }
 
     fn _code_of_laws() -> ShareFlow {
         Box::new(|player, _game| {
-            return ExecutingState::ChooseAnyCard {
+            return Some(State::Card {
                 min_num: 0,
                 max_num: Some(1),
                 from: player
@@ -119,26 +128,21 @@ mod tests {
                     .filter(|card| !player.board().borrow().get_stack(card.color()).is_empty())
                     .collect(),
                 callback: Box::new(move |cards| {
-                    match cards.get(0) {
-                        Some(&card) => {
-                            player.tuck(card);
-                            if !player.is_splayed(card.color(), Splay::Left) {
-                                ExecutingState::ChooseYn {
-                                    callback: Box::new(move |yn| {
-                                        if yn {
-                                            player.splay(card.color(), Splay::Left);
-                                        }
-                                        ExecutingState::Done
-                                    })
-                                }
-                            } else {
-                                ExecutingState::Done
-                            }
-                        },
-                        None => ExecutingState::Done,
+                    let card = *cards.get(0)?;
+                    player.tuck(card);
+                    if player.is_splayed(card.color(), Splay::Left) {
+                        return None;
                     }
-                })
-            };
+                    Some(State::Yn {
+                        callback: Box::new(move |yn| {
+                            if yn {
+                                player.splay(card.color(), Splay::Left);
+                            }
+                            None
+                        }),
+                    })
+                }),
+            });
         })
     }
 
