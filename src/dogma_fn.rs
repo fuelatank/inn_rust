58cc58dae26136e96913a5c7f38e2c8a09705c9f
Atank Fuel <fuelatank@gmail.com>
2022-11-27 13:26:21 +0800

    ideas

--- src/dogma_fn.rs
+++ src/dogma_fn.rs
@@ -8,6 +8,110 @@ use crate::{
     structure::Place,
 };
 
+/*
+struct Context {
+    s: Scope<RefChoice, ExecutionState>,
+}
+
+impl Context {
+    fn new(s: Scope<RefChoice, ExecutionState>) -> Context {
+        Context { s }
+    }
+
+    fn chooseOneCard(&mut self, player: &'g Player, from: Vec<&'c Card>) -> Option<Card> {
+        let cards = self.s
+            .yield_(ExecutionState::new(
+                player,
+                Choose::Card {
+                    min_num: 1,
+                    max_num: Some(1),
+                    from,
+                },
+            ))
+            .expect("Generator got None")
+            .cards();
+        debug_assert!(cards.length() <= 1);
+        cards.get(0)
+    }
+
+    fn chooseYn(&mut self, player: &'g Player) -> bool {
+        self.s.yield_(ExecutionState::new(
+            player,
+            Choose::Yn
+        )).expect("Generator got None").yn()
+    }
+
+    fn may<T, F>(&mut self, player: &'g Player, f: F) -> T
+    where F: FnOnce() -> T
+    {
+        self.chooseYn().then(f)
+    }
+}
+
+fn build_shared(f: F) -> ShareFlow
+where F: Fn(Context, &'g Player, &'g Players)
+{
+    |player, game| {
+        Gn::new_scoped_local(move |mut s|
+            let ctx = Context::shared(s);
+            f(player, game, ctx);
+            done!()
+        )
+    }
+}
+
+pub const AGRICULTURE: ShareFlow = shared(|player, game, ctx| {
+    let card = ctx.may(player, || ctx.chooseOneCard(player, player.hand));
+    card.then(|card| {
+        player.return(card);
+        player.draw_and_score(card.age());
+    });
+});
+
+fn f(&'a mut self) -> &'a Thing;
+
+// inner state
+// but need to ensure the execution order, or avoid multiple mutable borrows
+// i.e., ensure the first is dropped before the second is created
+// also, one dogma will be executed many times by different players
+// whether the state is shared is a question
+pub const OARS: impl Fn() -> Vec<Flow> = || {
+    let transferred: RcCell<bool> = RcCell::new(false);
+    vec![
+        // FnMut
+        demand(|player, opponent, game, ctx| {
+            let card = ctx.chooseOneCard(opponent, opponent.hand.has(crown));
+            card.then(|card| {
+                game.transfer(opponent.hand, player.score, card);
+                transferred.borrow_mut() = true;
+            });
+        }),
+        shared(|player, game, ctx| {
+            if !transferred.borrow() {
+                player.draw(1);
+            }
+        })
+    ]
+];
+
+// OR STRUCT?
+// create one in every execution
+// but that will cause waste
+// so what about RcCell?
+
+struct Oars { transferred: bool }
+impl Oars {
+}
+
+pub const OARS_1: DemandFlow = demand(|player, opponent, game, ctx| {
+    let card = yield ctx.chooseOneCard(opponent, opponent.hand.has(crown));
+    card.then(|card| {
+        game.transfer(opponent.hand, player.score, card);
+        env.set(true);
+    });
+})
+*/
+
 pub const ARCHERY: DemandFlow = |player, opponent, game| {
     Gn::new_scoped_local(move |mut s: Scope<RefChoice, _>| {
         game.draw(opponent, 1);
